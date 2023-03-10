56fea16c009f1fd7fe20b5c926718288fb8961dc
pwl1m <paulorsmalheiros@gmail.com>
2023-03-10 11:12:03 -0300

    first commit

---
+++ formatacaostring.py
@@ -0,0 +1,3 @@
+print(12,34,56, sep=",")
+print(12,84,39, sep=" ") #sep="" é um separador padrão de string
+print(12,84,39, sep=" ", end='\n') #end="" é um separador padrão de string para pular linha
